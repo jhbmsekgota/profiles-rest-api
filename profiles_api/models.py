9ade793a26eee416d7fed92bb2f2b94a0e13eced
makoma <makomasekgota@gmail.com>
2021-05-19 10:52:21 +0200

    Refractor

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """ Manager for user profiles"""
 
-    def create_user(self,email,name,password = None):
+    def create_user(self, email,name, password = None):
         """ Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
@@ -18,7 +18,7 @@ class UserProfileManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
-        return users
+        return user
 
     def create_superuser(self,email,name,password):
         """ Create and save a new supeuser with given details"""
@@ -44,7 +44,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FILEDS = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """ Retrieve full name of user"""
