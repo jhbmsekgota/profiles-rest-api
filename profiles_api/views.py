69a6dcfe36aa34f16af97272659d7c67a09e6566
makoma <makomasekgota@gmail.com>
2021-05-20 11:43:38 +0200

    Added helloworld api View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,31 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """ Test API View"""
     def get(self, request, format=None):
 
+        serializer_class = serializers.HelloSerializer
         """ Returns a list of APIView features"""
         an_apiview = ['Uses HTTP methods as function (get,post,patch,put,delete)',
         'Is similar to a traditional Django View',
         'Gives you the most contro over your application logic',
         'Is mapped manually to URLs',]
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+             )
