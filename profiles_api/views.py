27a034d6a2c85c5481a769f26792407d3817fb64
makoma <makomasekgota@gmail.com>
2021-05-21 08:12:11 +0200

    Added profiles viewsets

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -76,9 +79,19 @@ class HelloViewSet(viewsets.ViewSet):
         """ Handle updating an object """
         return Response({'http_method':'PUT'})
 
+
     def update(self,request,pk=None):
         """ Handle updating  a part of an object """
         return Response({'http_method':'PATCH'})
+
+
     def destroy(self,request,pk=None):
         """ Handle removing an object """
         return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
